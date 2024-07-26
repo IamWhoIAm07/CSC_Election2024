c8d8c236e357771138ecdc71485d3257808aa1ef
IamWhoIAm07 <92193081+IamWhoIAm07@users.noreply.github.com>
2024-07-26 20:29:53 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>St. Camillus College of Manaoag Foundation, Inc.</b>
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://www.facebook.com/people/SCCM-Central-Student-Council/61553938307666//">Central Student Council</a></strong>
+</footer>
